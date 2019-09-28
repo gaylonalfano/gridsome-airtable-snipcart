eab22a38c9abf94be85e8e2ba5a7a0ca7093446a
gaylonalfano <gaylon.alfano@gmail.com>
2019-09-28 21:41:44 +0800

    Added the ProductPreview and BuyButton components. Also updated GraphQL query with the new data shape. Created the Product template. Next, need to clean up the .product references to account for the new shape of the returned data. Also updated main.js to include Snipcart but need to update the data key as well.

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,4 +1,4 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
@@ -6,5 +6,13 @@
 
 module.exports = {
   siteName: 'Gridsome',
-  plugins: []
-}
+  plugins: [
+    {
+      use: '~/src/sources/products',
+      options: {
+        apiKey: process.env.AIRTABLE_API_KEY,
+        base: process.env.AIRTABLE_BASE_ID
+      }
+    }
+  ]
+};
--- src/main.js
+++ src/main.js
@@ -1,9 +1,30 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
+import DefaultLayout from '~/layouts/Default.vue';
 
-export default function (Vue, { router, head, isClient }) {
+export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component('Layout', DefaultLayout);
+
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css'
+  });
+
+  head.script.push({
+    type: 'text/javascript',
+    src: 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js',
+    body: true
+  });
+
+  head.script.push({
+    type: 'text/javascript',
+    src: 'https://cdn.snipcart.com/scripts/2.0/snipcart.js',
+    body: true,
+
+    // snipcart scripts' attributes
+    id: 'snipcart',
+    'data-api-key': 'MzMxN2Y0ODMtOWNhMy00YzUzLWFiNTYtZjMwZTRkZDcxYzM4'
+  });
 }
